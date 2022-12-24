3997be9f0e3f70ca5265cb0b94db0a683c12ad0e
tejaswinimukthapuram <tejaswinimukthapuram123@gmail.com>
2022-12-24 09:43:22 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {Component} from 'react'
+import Clock from './components/Clock'
+
+import './App.css'
+
+class App extends Component {
+  state = {showClock: false}
+
+  onButtonClick = () => {
+    this.setState(prevState => ({showClock: !prevState.showClock}))
+  }
+
+  render() {
+    const {showClock} = this.state
+    return (
+      <div className="app-container">
+        <button
+          type="button"
+          className="toggle-button"
+          onClick={this.onButtonClick}
+        >
+          {showClock ? 'Hide Clock' : 'Show Clock'}
+        </button>
+        {showClock && <Clock />}
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Clock/index.js
@@ -0,0 +1,35 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+    console.log('constructor Called')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount Called')
+    this.timerId = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount is called')
+    clearInterval(this.timerId)
+  }
+
+  tick = () => this.setState({date: new Date()})
+
+  render() {
+    const {date} = this.state
+    console.log('render Called')
+
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+export default Clock
